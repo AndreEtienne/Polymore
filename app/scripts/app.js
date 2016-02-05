22bd73bb4d298d8fbd24a0747e2344526ab750b6
Andre Etienne <andreetienne1987@gmail.com>
2016-02-05 11:00:53 +0100

    Update BaseUrl.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,11 +16,12 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   var app = document.querySelector('#app');
 
   // Sets app default base URL
-  app.baseUrl = '/';
-  if (window.location.port === '') {  // if production
+  app.baseUrl = '/Polymore/';
+  console.log(window.location.port);
+  if (window.location.port === '5000') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
-    // app.baseUrl = '/polymer-starter-kit/';
+     app.baseUrl = '/';
   }
 
   app.displayInstalledToast = function() {
