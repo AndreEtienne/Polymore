6fab2fdb6fa7a51b4265bada6953ccd202a26d81
Andre Etienne <andreetienne1987@gmail.com>
2016-02-22 10:36:01 +0100

    Change for gh-pages.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   var app = document.querySelector('#app');
 
   // Sets app default base URL
-  app.baseUrl = '/';
+  app.baseUrl = '';
   console.log(window.location.port);
   if (window.location.port === '') {  // if production
      app.baseUrl = '/Polymore/';
